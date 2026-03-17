56a349bc51875ccd431a430578c8dee7156e03dc
haicomet <hailiassommerville.com>
2026-03-17 15:52:17 -0400

    Chore: Database Schema

---
+++ server/database/db.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE areas (
+    pm_id VARCHAR(50) PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+CREATE TABLE grid (
+    grid_id VARCHAR(50) PRIMARY KEY,
+    x_pos INTEGER,
+    y_pos INTEGER,
+    pm_id VARCHAR(50) REFERENCES areas(pm_id),
+);
+
+CREATE TABLE work_order (
+    work_order_id VARCHAR(50) PRIMARY KEY,
+    pm_id VARCHAR(50) FOREIGN KEY REFERENCES areas(pm_id),
+    status VARCHAR(50),
+    target_start_date TIMESTAMP,
+    frequency VARCHAR(50),
+    description VARCHAR(255)
+);
