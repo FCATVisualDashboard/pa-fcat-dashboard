8165bd1abf5dcfbd80cd8e99e5733ec96ed82680
hailia <hailiassommerville@gmail.com>
2026-03-30 12:36:30 -0400

    fix vercel serverless export

--- server/index.js
+++ server/index.js
@@ -45,6 +45,10 @@ app.get('/api/workorders', async (req, res) => {
   }
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+module.exports = app;
